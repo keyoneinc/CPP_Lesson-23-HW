8984022b17cb3109efb7258c037a3bfd5ec75eef
Yaroslav <yaroslav@keyoneinc.uk>
2024-07-08 04:50:01 +0100

    added Bus classes methods defenition

--- hw23/Bus.cpp
+++ hw23/Bus.cpp
@@ -1,1 +1,30 @@
 #include "Bus.h"
+
+Bus::Bus(Time_ startTime, Time_ endTime, int routeDuration, int breakDuration)
+{
+    this->startTime = startTime;
+    this->endTime = endTime;
+    this->routeDuration = routeDuration;
+    this->breakDuration = breakDuration;
+    generateSchedule();
+}
+
+void Bus::generateSchedule()
+{
+    schedule.clear();
+    Time_ currentTime = startTime;
+
+    while (currentTime <= endTime) {
+        schedule.push_back(currentTime);
+        currentTime += routeDuration + breakDuration;
+    }
+}
+
+void Bus::showSchedule() const
+{
+    cout << "Bus schedule: " << endl;
+    for (const Time_& time : schedule) {
+        cout << time << " ";
+    }
+    cout << endl;
+}
