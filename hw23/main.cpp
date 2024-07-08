cd15e8fd8d0f25c2b89c8ba95bebad08edfacca0
Yaroslav <yaroslav@keyoneinc.uk>
2024-07-08 04:54:18 +0100

    added test code in main.cpp

--- hw23/main.cpp
+++ hw23/main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
+#include "Time_.h"
+#include "Bus.h"
 
 int main() {
+	Time_ startTime(6,0,0,"utc");
+	Time_ endTime(23,0,0,"utc");
+	int routeDuration = 1800; // time in seconds
+	int breakDuration = 600;
 
+	Bus bus(startTime, endTime, routeDuration, breakDuration);
+	bus.showSchedule();
+
+	return 0;
 }
